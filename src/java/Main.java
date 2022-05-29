5646ace5b972c5de7ff1c467f2a6953789abe6ca
Jafar <Jafar.E.Mortada@Gmail.com>
2022-05-29 15:40:52 +0100

    creating cards class
    constructor
    shuffle
    drawCard
    numberOfRemainingCards

--- src/java/Main.java
+++ src/java/Main.java
@@ -5,11 +5,6 @@ import src.java.deck.Suit;
 
 public class Main {
     public static void main(String[] args) {
-        for (Suit suit : Suit.values()) {         
-            for (int i = 1; i <= 13; i++) {                
-                Card card = new Card(i, suit);
-                System.out.println(card);
-            }   
-        }
+    	
     }
 }
---
+++ src/java/deck/Deck.java
@@ -0,0 +1,35 @@
+package src.java.deck;
+
+import java.util.ArrayList; // import the ArrayList class
+import java.util.Collections;
+
+public class Deck {
+	private ArrayList<Card> cards = new ArrayList<Card>();
+
+	public Deck() {
+		for (Suit suit : Suit.values()) {
+			for (int i = 1; i <= 13; i++) {
+				Card card = new Card(i, suit);
+				this.cards.add(card);
+			}
+		}
+	}
+
+	public void shuffle()
+	{
+		Collections.shuffle(this.cards);
+	}
+	public ArrayList<Card> drawCard(int N) {
+		ArrayList<Card> drawedCards = new ArrayList<Card>();
+		for (int i = 0; i < N; i++) {
+			drawedCards.add(this.cards.get(0));
+			this.cards.remove(0);
+		}
+		return drawedCards;
+	}
+
+	public int numberOfRemainingCards() {
+		return this.cards.size();
+	}
+
+}
