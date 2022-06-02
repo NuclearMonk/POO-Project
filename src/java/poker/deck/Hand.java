174c180a47ffd162d402b8fac0510d67ddbad27c
Jafar <Jafar.E.Mortada@Gmail.com>
2022-06-02 13:22:03 +0100

    Removing comment (Jafar's original hand)

--- src/java/poker/deck/Hand.java
+++ src/java/poker/deck/Hand.java
@@ -42,36 +42,6 @@ public class Hand {
 		}
 	}
 
-	// public Hand(List<Card> list) {
-	// if (list.size() != 5)
-	// throw new IllegalArgumentException("Hands should be only 5 cards");
-	// this.cards.addAll(list);
-	// }
-
-	// public void replaceCards(Deck deck1, int a[]) {
-
-	// if (a.length > 5 || Arrays.stream(a).max().getAsInt() > 5 ||
-	// Arrays.stream(a).min().getAsInt() < 1)
-	// throw new IllegalArgumentException("Illegal indexes");
-	// int numberOfCards = 5 - a.length;
-	// ArrayList<Card> cardsFromDeck = new
-	// ArrayList<Card>(deck1.drawCard(numberOfCards));
-	// boolean replace = true;
-	// for (int i = 1; i < 6; i++)
-
-	// {
-	// replace = true;
-	// for (int element : a) {
-	// if (element == i) {
-	// replace = false;
-	// }
-	// }
-	// if (replace)
-	// this.cards.set((i - 1), cardsFromDeck.remove(0));
-	// }
-
-	// }
-
 	@Override
 	public String toString() {
 		return "Hand = " + Arrays.toString(this.cards);
