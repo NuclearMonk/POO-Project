1c4ab2989b459102b0860a79c460d28e76f9e371
Jafar <Jafar.E.Mortada@Gmail.com>
2022-06-14 22:11:42 +0100

    ACE

---
+++ src/java/poker/app/hand/analyzer/Ace.java
@@ -0,0 +1,33 @@
+package src.java.poker.app.hand.analyzer;
+
+import java.util.List;
+
+import src.java.poker.app.hand.Hand;
+import src.java.poker.app.hand.recognition.HandRecognitionResult;
+import src.java.poker.app.hand.recognition.HandRecognizer;
+import src.java.poker.card.Card;
+import src.java.poker.player.actions.HoldCardsAction;
+import src.java.poker.player.actions.PlayerAction;
+
+public class Ace extends HandRecognizer implements HandAction {
+
+	protected Ace() {
+		super("ACE", 0);
+	}
+
+	@Override
+	public PlayerAction getAdviceAction(Hand hand) {
+		List<Integer> indexes = hand.getCardIndex(this.recognizeHand(hand).getDefiningCard().getValue(),
+				this.recognizeHand(hand).getDefiningCard().getSuit());
+		return new HoldCardsAction(indexes);
+	}
+
+	@Override
+	public HandRecognitionResult recognizeHand(Hand hand) {
+		for (Card C : hand.cardsAsList()) {
+			if (C.getValue() == Card.ACE)
+				return new HandRecognitionResult(true, C);
+		}
+		return new HandRecognitionResult(false, null);
+	}
+}
