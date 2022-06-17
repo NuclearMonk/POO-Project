c8d56ee1cb0f33daf6ec9f01dbb7bafd937c8e06
Jafar <Jafar.E.Mortada@Gmail.com>
2022-06-17 09:44:37 +0100

    Create KQJUnsuited.java

---
+++ src/java/poker/app/hand/recognition/missingcards/straight/KQJUnsuited.java
@@ -0,0 +1,25 @@
+package src.java.poker.app.hand.recognition.missingcards.straight;
+
+import java.util.List;
+
+import src.java.poker.app.hand.Hand;
+import src.java.poker.app.hand.recognition.HandRecognitionResult;
+import src.java.poker.card.Card;
+
+public class KQJUnsuited extends ThreeToStraight {
+
+	@Override
+	public HandRecognitionResult recognizeHand(Hand hand) {
+		HandRecognitionResult superResult = super.recognizeHand(hand);
+		if (superResult.isResult()) {
+			List<Integer> values = getStraightMembers(hand);
+			for (int i = 0; i < 3; i++) {
+				if (i < Card.JACK)
+					return new HandRecognitionResult(false, null);
+			}
+			return superResult;
+		}
+		return new HandRecognitionResult(false, null);;
+	}
+
+}
